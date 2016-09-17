6f2af953382a8e2e5777a8819c8db567b048d2d6
gail64917 <gail64917@yandex.ru>
2016-09-17 11:40:17 +0300

    [upd] добавлена хранимая процедура (не удалена старая, крашнутая бд 642)

---
+++ QuestGame.WebApi/ADO.NET/dbo.GetQuestsByUserRole.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[GetQuestsByUserRole]
+	@roleName nvarchar(100)
+AS
+	SELECT * 
+	FROM Quests
+	WHERE Quests.OwnerId IN
+		(SELECT u.UserId
+		FROM AspNetUserRoles u
+		INNER JOIN AspNetRoles
+		ON r.Id=u.RoleId AND r.name=@roleName)
+GO
