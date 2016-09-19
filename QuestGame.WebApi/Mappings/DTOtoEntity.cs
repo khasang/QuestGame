43e50da785b4070eba097501071b1d659b1538f4
KloderGit <kloder3@gmail.com>
2016-09-19 17:21:05 +0300

    DTO - Entity

---
+++ QuestGame.WebApi/Mappings/DTOtoEntity.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using QuestGame.Domain.DTO;
+using QuestGame.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QuestGame.WebApi.Mappings
+{
+    public class DTOtoEntity : Profile
+    {
+        public DTOtoEntity()
+        {
+            CreateMap<StageDTO, Stage>();
+        }
+    }
+}
