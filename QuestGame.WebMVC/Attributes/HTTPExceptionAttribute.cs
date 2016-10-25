95a366e5d24d12a457e77dbdd3bcd3312b38ea84
KloderGit <kloder3@gmail.com>
2016-10-25 14:08:25 +0300

    Добавил HTTPExceptionAttribute

---
+++ QuestGame.WebMVC/Attributes/HTTPExceptionAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net.Http;
+
+namespace QuestGame.WebMVC.Attributes
+{
+    public class HTTPExceptionAttribute : FilterAttribute, IExceptionFilter
+    {
+        public void OnException(ExceptionContext filterContext)
+        {
+            if (!filterContext.ExceptionHandled && filterContext.Exception is HttpRequestException)
+            {
+                filterContext.Result =
+                    new RedirectResult("~/Content/RangeErrorPage.html");
+                filterContext.ExceptionHandled = true;
+            }
+        }
+    }
+}
+
