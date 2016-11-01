11cfcdb509ba7f1ddb9490a056f3c2d2a7a20796
KloderGit <kloder3@outlook.com>
2016-11-02 01:16:42 +0300

    Черновик фабрики: Абстрактный класс класс для Googl
    Создание класса из имени

--- QuestGame.WebMVC/Controllers/ExternalLoginController.cs
+++ QuestGame.WebMVC/Controllers/ExternalLoginController.cs
@@ -21,6 +21,14 @@ namespace QuestGame.WebMVC.Controllers
         // GET: ExternalLogin
         public ActionResult Index()
         {
+            Type T = Type.GetType("QuestGame.WebMVC.Controllers.GoogleAuth");
+
+            object Obj = Activator.CreateInstance(T);
+
+            SocialProvider Prov = (SocialProvider)Obj;
+
+            var t = Prov.RequestAuth;
+
             var googleUrl = "https://accounts.google.com/o/oauth2/auth" +
                 "?response_type=code" +
                 "&client_id=803183701728-q1ktbmuhces4vdj9udkmatn0gota8he8.apps.googleusercontent.com" +
@@ -92,4 +100,82 @@ namespace QuestGame.WebMVC.Controllers
             return RedirectToAction("Index", "Home");
         }
     }
+
+
+    public abstract class SocialProvider
+    {
+        public string AccessToken { get; set; }
+        public string Code { get; set; }
+        public string RedirectUri { get; set; }
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+        public string ApplicationAuthPath { get; set; }
+        public string ApplicationAuthTokenPath { get; set; }
+        public string RequestUserInfoPath { get; set; }
+
+        public virtual string RequestAuth
+        {
+            get
+            {
+                var uriBuilder = new UriBuilder(this.ApplicationAuthPath);
+                var parameters = HttpUtility.ParseQueryString(string.Empty);
+                parameters["response_type"] = "code";
+                parameters["client_id"] = this.ClientId;
+                parameters["redirect_uri"] = this.RedirectUri;
+                parameters["scope"] = "openid%20profile%20email";
+                uriBuilder.Query = parameters.ToString();
+                return uriBuilder.Uri.ToString();
+            }
+        }
+
+        public virtual string RequestToken
+        {
+            get
+            {
+                var uriBuilder = new UriBuilder(this.ApplicationAuthTokenPath);
+                var parameters = HttpUtility.ParseQueryString(string.Empty);
+                parameters["client_id"] = this.ClientId;
+                parameters["client_secret"] = this.ClientSecret;
+                parameters["redirect_uri"] = this.RedirectUri;
+                parameters["grant_type"] = "authorization_code";
+                parameters["code"] = this.Code;
+                uriBuilder.Query = parameters.ToString();
+                return uriBuilder.Uri.ToString();
+            }
+        }
+
+        public virtual string RequestUserInfo
+        {
+            get
+            {
+                var uriBuilder = new UriBuilder(this.RequestUserInfoPath);
+                var parameters = HttpUtility.ParseQueryString(string.Empty);
+                parameters["access_token"] = this.AccessToken;
+                uriBuilder.Query = parameters.ToString();
+                return uriBuilder.Uri.ToString();
+            }
+        }
+
+
+
+    }
+
+    public class GoogleAuth : SocialProvider
+    {
+        public GoogleAuth()
+        {
+            this.RedirectUri = "https://localhost:44366/ExternalLogin/GoogleCode";
+            this.ClientId = "803183701728-q1ktbmuhces4vdj9udkmatn0gota8he8.apps.googleusercontent.com";
+            this.ClientSecret = "yXNNeyD0OlL7pS-yfSzGL4bv";
+            this.ApplicationAuthPath = "https://accounts.google.com/o/oauth2/auth";
+            this.ApplicationAuthTokenPath = "https://accounts.google.com/o/oauth2/token";
+            this.RequestUserInfoPath = "https://www.googleapis.com/oauth2/v1/userinfo";
+        }
+
+        public string ProviderName()
+        {
+            return "Google";
+        }
+    }
+
 }
