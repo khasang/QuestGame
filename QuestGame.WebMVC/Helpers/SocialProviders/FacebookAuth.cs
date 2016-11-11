2be9b80361ff9832e797882b2b0d4b47e025d393
KloderGit <kloder3@gmail.com>
2016-11-11 16:02:38 +0300

    реализовал FaceBook

--- QuestGame.WebMVC/Helpers/SocialProviders/FacebookAuth.cs
+++ QuestGame.WebMVC/Helpers/SocialProviders/FacebookAuth.cs
@@ -3,6 +3,12 @@ using System.Web;
 using System.Web.Services;
 using System.Web.Services.Protocols;
 using System.ComponentModel;
+using System.Web.Configuration;
+using QuestGame.WebMVC.Models;
+using QuestGame.Common.Helpers;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
 
 namespace QuestGame.WebMVC.Helpers.SocialProviders
 {
@@ -11,40 +17,92 @@ namespace QuestGame.WebMVC.Helpers.SocialProviders
 
         public FacebookAuth()
         {
-            appParams = new SocialAppParams
-            {
-                ClientId = "1601644850130436",
-                ClientSecret = "5ca60a2235c69ed57cb4aa43685e84cc",
-                RedirectUri = "https://localhost:44366/ExternalLogin/FaceBookAuthCallback",
-                Scope = "id,email,name,picture.width(200).height(200)",
-                Provider = "FaceBook"
-            };
+            this.Provider = WebConfigurationManager.AppSettings["FaceBookProvider"];
 
-            appPaths = new SocialAppPaths
-            {
-                AppGetCodePath = "https://www.facebook.com/v2.8/dialog/oauth",
-                AppGetTokenPath = "https://graph.facebook.com/v2.8/oauth/access_token",
-                AppGetUserInfoPath = "https://graph.facebook.com/me/"
-            };
+            ClientId = WebConfigurationManager.AppSettings[this.Provider + "ClientId"];
+            ClientSecret = WebConfigurationManager.AppSettings[this.Provider + "ClientSecret"];
+            RedirectUri = WebConfigurationManager.AppSettings[this.Provider + "RedirectUri"];
+            Scope = WebConfigurationManager.AppSettings[this.Provider + "Scope"];
 
-            getToken = new GetFaceBookToken (appParams, appPaths);
-            getUserInfo = new GetFaceBookUserInfo(appParams, appPaths);
+            AppGetCodePath = WebConfigurationManager.AppSettings[this.Provider + "AppGetCodePath"];
+            AppGetTokenPath = WebConfigurationManager.AppSettings[this.Provider + "AppGetTokenPath"];
+            AppGetUserInfoPath = WebConfigurationManager.AppSettings[this.Provider + "AppGetUserInfoPath"];
         }
 
         public override string RequestCodeUrl
         {
             get
             {
-                var uriBuilder = new UriBuilder(this.appPaths.AppGetCodePath);
+                var uriBuilder = new UriBuilder(this.AppGetCodePath);
                 var parameters = HttpUtility.ParseQueryString(string.Empty);
                 parameters["response_type"] = "code";
-                parameters["client_id"] = this.appParams.ClientId;
-                parameters["redirect_uri"] = this.appParams.RedirectUri;
+                parameters["client_id"] = this.ClientId;
+                parameters["redirect_uri"] = this.RedirectUri;
                 //parameters["scope"] = this.appParams.Scope;
                 uriBuilder.Query = parameters.ToString();
                 return uriBuilder.Uri.ToString();
             }
         }
 
+        public override SocialUserModel GetUserInfo()
+        {
+            var uriBuilder = new UriBuilder(this.AppGetUserInfoPath);
+            var parameters = HttpUtility.ParseQueryString(string.Empty);
+            parameters["access_token"] = this.AccessToken;
+            parameters["fields"] = this.Scope;
+            uriBuilder.Query = parameters.ToString();
+            var queryUrl = uriBuilder.Uri.ToString();
+
+            using (var client = RestHelper.Create())
+            {
+                var request = client.GetAsync(queryUrl).Result;
+                var answer = request.Content.ReadAsStringAsync().Result;
+                dynamic json = JsonConvert.DeserializeObject(answer);
+
+                string avatar;
+
+                bool pictureIsAviable = (bool)json.picture.data.is_silhouette;
+
+                if (!pictureIsAviable)
+                {
+                    avatar = json.picture[0].data[0].url;
+                }
+                else
+                {
+                    avatar = @"http://vignette3.wikia.nocookie.net/shokugekinosoma/images/6/60/No_Image_Available.png/revision/latest?cb=20150708082716";
+                }
+
+                return new SocialUserModel
+                {
+                    SocialId = json.id,
+                    Email = json.email,
+                    Password = json.id,
+                    NickName = json.name,
+                    AvatarUrl = avatar,
+                    Provider = this.Provider
+                };
+            }
+        }
+
+        protected override string GetToken()
+        {
+            var uriBuilder = new UriBuilder(this.AppGetTokenPath);
+            var parameters = HttpUtility.ParseQueryString(string.Empty);
+            parameters["client_id"] = this.ClientId;
+            parameters["redirect_uri"] = this.RedirectUri;
+            parameters["client_secret"] = this.ClientSecret;
+            parameters["code"] = this.Code;
+            uriBuilder.Query = parameters.ToString();
+
+            var queryUrl = uriBuilder.Uri.ToString();
+
+            using (var client = RestHelper.Create())
+            {
+                var response = client.GetAsync(queryUrl).Result;
+                var responseData = response.Content.ReadAsAsync<Dictionary<string, string>>().Result;
+
+                return responseData["access_token"];
+            }
+        }
     }
 }
