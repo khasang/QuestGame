dda492057a8a84ff6ed6cbac9a0e2bbe0d486d08
KloderGit <kloder3@outlook.com>
2016-11-16 20:42:24 +0300

    Настройка модели данных социального пользователя для получения на вебапи

--- QuestGame.Domain/DTO/SocialUserDTO.cs
+++ QuestGame.Domain/DTO/SocialUserDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,11 +9,20 @@ namespace QuestGame.Domain.DTO
 {
     public class SocialUserDTO
     {
+        [Required]
         public string SocialId { get; set; }
+
         public string NickName { get; set; }
+
+        [Required]
         public string Email { get; set; }
+
+        [Required]
         public string Password { get; set; }
+
         public string AvatarUrl { get; set; }
+
+        [Required]
         public string Provider { get; set; }
     }
 }
